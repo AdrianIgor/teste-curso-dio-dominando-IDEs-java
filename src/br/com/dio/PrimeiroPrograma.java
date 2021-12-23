e089f9824d635028c44a2d4c5e422cbafc7b2433
Adrian Igor <desenevil07@gmail.com>
2021-12-23 16:08:45 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*int a = 321;
+		int b = 543;
+		System.out.println("Olá, Mundo! " + (a+b));*/
+	}
+	
+}
+
+class Livros{
+	private String nome;
+	private int paginas;
+}
